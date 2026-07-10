cc3f04fae64f32789860d8281535d443e49e3571
gabriele-chiocca <gabrielechiocca00@gmail.com>
2026-07-10 19:27:08 +0200

    Git init + first structure final project

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="container mt-5">
+      <h1>Ciao</h1>
+    </div>
+  );
+}
+
+export default App;
