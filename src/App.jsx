e1456a05811a001aec83999437754eede6727036
gabriele-chiocca <gabrielechiocca00@gmail.com>
2026-07-11 12:11:21 +0200

    structure browser router and pages in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,30 @@
 import { useState } from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
+import Header from './components/Header';
+import Footer from './components/Footer';
+
+import Home from './pages/Home';
+import AnimeDetail from './pages/AnimeDetail';
+import Favorites from './pages/Favorites';
+import Compare from './pages/Compare';
 
 function App() {
   return (
-    <div className="container mt-5">
-      <h1>Ciao</h1>
-    </div>
+    <BrowserRouter>
+      <Header />
+
+      <main>
+        <Routes>
+          <Route path="/" element={Home} />
+          <Route path="/anime/:id" element={AnimeDetail} />
+          <Route path="/favorites" element={Favorites} />
+          <Route path="/compare" element={Compare} />
+        </Routes>
+      </main>
+
+      <Footer />
+    </BrowserRouter>
   );
 }
 
