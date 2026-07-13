29805deca848fad070389e3e900afb80e8582f6e
gabriele-chiocca <gabrielechiocca00@gmail.com>
2026-07-13 20:09:48 +0200

    added filter for Order A-Z  & Z-A

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -9,6 +9,7 @@ function Home() {
 
   const [search, setSearch] = useState('');
   const [selectedCategory, setSelectedCategory] = useState('');
+  const [sortOrder, setSortOrder] = useState('A-Z');
 
   useEffect(() => {
     async function loadAnimes() {
@@ -33,19 +34,25 @@ function Home() {
     return <p>{error}</p>;
   }
 
-  const filteredAnimes = animes.filter((anime) => {
-    const formattedTitleAnime = anime.title.toLowerCase();
+  const filteredAnimes = animes
+    .filter((anime) => {
+      const formattedTitleAnime = anime.title.toLowerCase();
 
-    const formattedInput = search.toLowerCase();
+      const formattedInput = search.toLowerCase();
 
-    const matchesTitle = formattedTitleAnime.includes(formattedInput);
+      const matchesTitle = formattedTitleAnime.includes(formattedInput);
 
-    const matchesCategory =
-      selectedCategory === '' || anime.category === selectedCategory;
+      const matchesCategory =
+        selectedCategory === '' || anime.category === selectedCategory;
 
-    return matchesTitle && matchesCategory;
-  });
-
+      return matchesTitle && matchesCategory;
+    })
+    .sort((a, b) => {
+      if (sortOrder === 'Z-A') {
+        return b.title.localeCompare(a.title);
+      }
+      return a.title.localeCompare(b.title);
+    });
   return (
     <div className="container py-4">
       <h1 className="mb-4">Anime</h1>
@@ -60,24 +67,38 @@ function Home() {
         <i className="fas fa-search search-icon"></i>
       </div>
 
-      <div className="mb-4">
-        <select
-          className="form-select"
-          aria-label="Default select example"
-          placeholder="Scegli la categoria"
-          onChange={(e) => setSelectedCategory(e.target.value)}
-          value={selectedCategory}
-        >
-          <option value="">Categorie</option>
-          <option value="Shonen">Shonen</option>
-          <option value="Psychological">Psychological</option>
-          <option value="Adventure">Adventure</option>
-          <option value="Seinen">Seinen</option>
-          <option value="Isekai">Isekai</option>
-          <option value="Sports">Sports</option>
-        </select>
-      </div>
       <div className="row g-4">
+        <div className="mb-4 col-6">
+          <select
+            className="form-select"
+            aria-label="Default select example"
+            placeholder="Scegli la categoria"
+            onChange={(e) => setSelectedCategory(e.target.value)}
+            value={selectedCategory}
+          >
+            <option value="">Filter Category</option>
+            <option value="Shonen">Shonen</option>
+            <option value="Psychological">Psychological</option>
+            <option value="Adventure">Adventure</option>
+            <option value="Seinen">Seinen</option>
+            <option value="Isekai">Isekai</option>
+            <option value="Sports">Sports</option>
+          </select>
+        </div>
+
+        <div className="col-6">
+          <select
+            className="form-select col-6"
+            aria-label="Default select example"
+            placeholder="Scegli la categoria"
+            onChange={(e) => setSortOrder(e.target.value)}
+            value={sortOrder}
+          >
+            <option value="A-Z">A-Z</option>
+            <option value="Z-A">Z-A</option>
+          </select>
+        </div>
+
         {filteredAnimes.map((anime) => {
           return <AnimeCard anime={anime} key={anime.id} />;
         })}
