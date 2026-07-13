5f08014cd288328399ce989fcf6afd465d20d315
gabriele-chiocca <gabrielechiocca00@gmail.com>
2026-07-13 17:37:27 +0200

    structure simple list api call

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from 'react';
 import { getAllAnimeDetails } from '../services/animeApi';
 
 function Home() {
-  const [anime, setAnime] = useState([]);
+  const [animes, setAnimes] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState('');
 
@@ -10,7 +10,7 @@ function Home() {
     async function loadAnimes() {
       try {
         const data = await getAllAnimeDetails();
-        setAnime(data);
+        setAnimes(data);
       } catch (error) {
         setError(error.message);
       } finally {
@@ -18,10 +18,44 @@ function Home() {
       }
     }
 
-    return () => {};
+    loadAnimes();
   }, []);
 
-  return <h1>Home</h1>;
+  if (loading) {
+    return <p>Caricamento Anime</p>;
+  }
+
+  if (error) {
+    return <p>{error}</p>;
+  }
+
+  return (
+    <div className="container py-4">
+      <h1 className="mb-4">Anime</h1>
+      <div className="row g-4">
+        {animes.map((anime) => {
+          return (
+            <div className="col-12 col-md-6 col-lg-4" key={anime.id}>
+              <div className="card h-100">
+                <img
+                  src={anime.image}
+                  alt={anime.title}
+                  className="card-img-top"
+                />
+
+                <div className="card-body">
+                  <h2 className="h5">{anime.title}</h2>
+                  <p>{anime.category}</p>
+                  <p>Rating: {anime.rating}</p>
+                  <p>{anime.description}</p>
+                </div>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
 }
 
 export default Home;
